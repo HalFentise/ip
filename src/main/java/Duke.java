c7637addc0a0ed28819dd146ab22254cd3c86624
HalFentise <2837713859@qq.com>
2025-01-30 14:47:21 +0800

    level0

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,8 @@
 public class Duke {
     public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+        System.out.println("--------------------------------");
+        System.out.println("Hello, I am Martin!\nWhat can I do for you?");
+        System.out.println("--------------------------------");
+        System.out.println("Bye, see you next time!");
     }
 }
