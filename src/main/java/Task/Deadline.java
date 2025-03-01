dd2702f44a4eb7ac5dc8949f00a59e7172109529
HalFentise <2837713859@qq.com>
2025-03-01 14:38:54 +0800

    fix deadline

--- src/main/java/Task/Deadline.java
+++ src/main/java/Task/Deadline.java
@@ -1,29 +1,59 @@
 package Task;
 
-import Exceptions.*;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.time.format.DateTimeParseException;
 
 public class Deadline extends Task {
-    private String deadline;
+    private String deadlineTime;
+    private LocalDate date = null;
+    private LocalDateTime dateTime = null;
 
-    public Deadline(String taskString) {
-        super(taskString);
-        String[] parts = taskString.split(" by ");
-        if (parts.length == 2) {
-            setTaskName(parts[0]);
-            this.deadline = parts[1];
-        } else {
-            throw new IllegalArgumentException("Invalid event time format.");
+    public Deadline(String taskName, String deadline) {
+        super(taskName);
+        this.deadlineTime = deadline;
+        parseInput(deadline);
+    }
+
+    private void parseInput(String string) {
+        //define time format
+        String[] dateTimeFormats = {
+                "d/M/yyyy HHmm",
+                "d/M/yyyy",
+        };
+
+        for (String format : dateTimeFormats) {
+            try {
+                DateTimeFormatter formatter = DateTimeFormatter.ofPattern(format);
+                if (format.contains("HHmm")) { //if contain time
+                    this.dateTime = LocalDateTime.parse(string, formatter);
+                } else { //only contain date
+                    this.date = LocalDate.parse(string, formatter);
+                }
+                return;
+            } catch (DateTimeParseException e) {
+                //ignore exception
+            }
         }
     }
 
+    @Override
     public String toString() {
-        String state = isDone ? "[X]" : "[ ]";
-        return "[D]" + state + " " + taskName + " (by:" + deadline + " )";
+        if (dateTime != null) {
+            DateTimeFormatter formatter = DateTimeFormatter.ofPattern("MMM dd yyyy, h:mm a");
+            return "[D]" + super.toString() + " (by: " + dateTime.format(formatter) + ")";
+        } else if (date != null) {
+            DateTimeFormatter formatter = DateTimeFormatter.ofPattern("MMM dd yyyy");
+            return "[D]" + super.toString() + " (by: " + date.format(formatter) + ")";
+        } else {
+            return "[D]" + super.toString() + " (by: " + deadlineTime + ")";
+        }
     }
 
     @Override
     public String toFileFormat() {
-        return "D | " + (isDone ? "  Done  " : "Not Done") + " | " + getTaskName() + " (by: " + deadline + ")";
+        return "D | " + (isDone ? "  Done  " : "Not Done") + " | " + getTaskName() + " (by: " + deadlineTime + ")";
     }
 
     static public Task fromFileFormat(String fileFormat) {
